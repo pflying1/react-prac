26b92923fc5d5f8be74bcfe656c20ab7ba2ad5d2
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-05-24 12:10:31 +0900

    라우터 돔 설정

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,46 +1,19 @@
-/* import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-export default App; */
-
-function MyButton() {
-  return <button>I'm a button</button>;
-}
-function DivisionTwo() {
-  return (
-    <div>
-      <div>division 이 한개</div>
-      <div>division 이 두개</div>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default function MyApp() {
-  return (
-    <div>
-      <div>안녕하세요</div>
-      <MyButton />
-      <DivisionTwo />
-    </div>
-  );
-}
+export default App;
