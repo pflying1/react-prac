17f2e1ae3914c7e3c6d8e768bd11d6f86ae54f16
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-06-01 17:33:03 +0900

    비구조화 할당 문법을 통해 props 내부 값 추출하기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import NotFound from "./components/NotFound";
+import App2 from "./components/App2";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/app2" element={<App2 />} />
+    </Routes>
   );
 }
 
---
+++ my-app/src/components/App2.js
@@ -0,0 +1,7 @@
+import MyComponent from "./MyComponent";
+
+const App2 = () => {
+  return <MyComponent name={"홍"}>리액트</MyComponent>;
+};
+
+export default App2;
