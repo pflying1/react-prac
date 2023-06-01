efd2aa822924aa9fda77b724b17971f648137e4a
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-06-01 17:46:13 +0900

    index 부분에 라우터가 있어서 계속 에러가 나왔었다.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import NotFound from "./components/NotFound";
@@ -8,7 +8,7 @@ import App2 from "./components/App2";
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
       <Route path="/app2" element={<App2 />} />
